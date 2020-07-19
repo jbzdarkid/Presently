3c51816712dda5a52e89b8cb45618063a8500ef3
jbzdarkid <jbzdarkid@gmail.com>
2020-07-18 23:59:26 -0700

    more settings work

--- settings.js
+++ settings.js
@@ -38,7 +38,7 @@ function hideSettings() {
   }, 1000)
 }
 
-// Load settings
+// Load settings on page load
 document.addEventListener('DOMContentLoaded', function() {
   addSetting("Temperature", "Farenheit", "Celsius")
   addSetting("Hours", "12", "24")
@@ -51,9 +51,19 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 })
 
+var units = 'F'
+var normalizedUnits = function(degreesF) {
+  if (units == 'F') {
+    return degreesF
+  } else {
+    var deg = (parseInt(degreesF) - 32) * 5
+    return Math.floor(deg / 9)
+  }
+}
+
 function addSetting(titleText, option1Text, option2Text) {
   var div = document.createElement('div')
-  document.getElementById('settings-list-placeholder').appendChild(div)
+  document.getElementById('settings-list').appendChild(div)
 
   var title = document.createElement('label')
   div.appendChild(title)
@@ -62,17 +72,21 @@ function addSetting(titleText, option1Text, option2Text) {
   title.innerText = titleText
 
   div.appendChild(document.createElement('br'))
+  div.appendChild(document.createElement('br'))
 
   var option1button = document.createElement('input')
   div.appendChild(option1button)
-  option1button.name = titleText
+  option1button.name = window.localize(titleText, titleText)
   option1button.type = 'radio'
   option1button.id = titleText + '-' + option1Text
+  option1button.onchange = function() {
+    window.setRemote('settings-' + titleText, option1Text)
+  }
 
   var option1label = document.createElement('label')
   div.appendChild(option1label)
   option1label.for = option1button.id
-  option1label.innerText = option1Text
+  option1label.innerText = window.localize(option1Text, option1Text)
   option1label.style.fontFamily = 'OpenSans-Light'
   option1label.style.fontSize = '20px'
 
@@ -81,11 +95,22 @@ function addSetting(titleText, option1Text, option2Text) {
   option2button.name = titleText
   option2button.type = 'radio'
   option2button.id = titleText + '-' + option2Text
+  option2button.onchange = function() {
+    window.setRemote('settings-' + titleText, option2Text)
+  }
 
   var option2label = document.createElement('label')
   div.appendChild(option2label)
   option2label.for = option2button.id
-  option2label.innerText = option2Text
+  option2label.innerText = window.localize(option2Text, option2Text)
   option2label.style.fontFamily = 'OpenSans-Light'
   option2label.style.fontSize = '20px'
+
+  window.getRemote('settings-' + titleText, function(value) {
+    if (value == undefined || value == option1Text) {
+      option1button.checked = true
+    } else {
+      option2button.checked = true
+    }
+  })
 }
